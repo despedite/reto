dd9352e6f217c96f487537ccb68c105241668fc4
Erik Bianco Vera <biancoveraerik@gmail.com>
2020-06-10 11:11:11 -0300

    Misc. updates for v1.4

--- reto.py
+++ reto.py
@@ -9,6 +9,7 @@ import aiofiles
 import os.path
 import os
 import json
+import random
 
 db = TinyDB('db.json') #Database file: stores points of every user.
 cfg = TinyDB("config.json") #Config file: stores configurations for the bot. Modify at your heart's content!
@@ -23,7 +24,7 @@ for c in config:
 	prefix = c['prefix']
 	botactivity = c['activity']
 
-bot = commands.Bot(command_prefix=prefix)
+bot = commands.Bot(command_prefix=prefix, connector=aiohttp.TCPConnector(ssl=False))
 client = discord.Client()
 ascii_banner = pyfiglet.figlet_format(botname + ".py")
 
@@ -49,17 +50,28 @@ async def on_ready():
 	else:
 		print ("Running with the name " + str(bot.user) + " on " + str(len(bot.guilds)) + " servers")
 	print ("Invite link: https://discordapp.com/api/oauth2/authorize?client_id=" + str(bot.user.id) + "&permissions=1342449744&scope=bot")
+	print ("Ver " + botver + " - check the updates with ?changelog")
 	print ("?setup to get started!")
 	print ("--------------------------------------------")
 	if botver != "":
-		game = discord.Game(botactivity + " | " + botver)
+		game = discord.Game(botactivity[random.randrange(len(botactivity))] + " | v" + botver)
 	else:
 		game = discord.Game(botactivity)
 	await bot.change_presence(activity=game)
+	bot.loop.create_task(statusupdates())
 
 @bot.event
 async def on_guild_join(guild):
+	#NICE
 	srv.insert({'serverid': guild.id, 'heart': 'plus', 'crush': 'minus', 'star': '10'})
 
+async def statusupdates():
+	while True:
+		await asyncio.sleep(30)
+		if botver != "":
+			game = discord.Game(botactivity[random.randrange(len(botactivity))] + " | v" + botver)
+		else:
+			game = discord.Game(botactivity)
+		await bot.change_presence(activity=game)
 
 bot.run(bottoken)
