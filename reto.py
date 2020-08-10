db9114faaca440235157b4254667a6da9f168120
Erik Bianco Vera <biancoveraerik@gmail.com>
2020-08-10 00:46:49 -0300

    Reto v1.5!

--- reto.py
+++ reto.py
@@ -10,20 +10,49 @@ import os.path
 import os
 import json
 import random
+import logging
+import dbl
 
-db = TinyDB('db.json') #Database file: stores points of every user.
-cfg = TinyDB("config.json") #Config file: stores configurations for the bot. Modify at your heart's content!
-srv = TinyDB('server.json') #Server-specific configuration - allows you to modify stuff like the name of the reactions, for example.
+db = TinyDB('json/db.json') #Database file: stores points of every user.
+cfg = TinyDB("json/config.json") #Config file: stores configurations for the bot. Modify at your heart's content!
+srv = TinyDB('json/server.json') #Server-specific configuration - allows you to modify stuff like the name of the reactions, for example.
+activity = TinyDB('json/activity.json') #Activity file: the "Playing" commands the bot has.
+customprefix = TinyDB('json/prefix.json') #Prefix file: custom prefixes per server.
 
-for c in cfg:
+# Code for logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='reto.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+config = cfg.search(Query()['search'] == 'value')
+for c in config:
 	bottoken = c['bottoken']
 	botname = c['botname']
-	devname = c['devname']
+	support = c['support']
 	botver = c['botver']
 	prefix = c['prefix']
-	botactivity = c['activity']
 
-bot = commands.Bot(command_prefix=prefix, connector=aiohttp.TCPConnector(ssl=False))
+getactivities = activity.all()
+botactivity = []
+i = 0
+for value in getactivities:
+	botactivity.append(getactivities[i]["activity"])
+	i += 1	
+
+def get_prefix(bot, msg):
+	customprefix.clear_cache()
+	if not msg.guild: # is on DMs
+		return commands.when_mentioned_or(prefix)(bot,msg)
+	else:
+		pre = customprefix.search(Query().server == msg.guild.id)
+		if pre:
+			return pre[0]["prefix"] # custom prefix on json db
+		else:
+			return prefix # default prefix
+
+bot = commands.Bot(command_prefix=get_prefix)
 client = discord.Client()
 ascii_banner = pyfiglet.figlet_format(botname + ".py")
 
@@ -39,7 +68,6 @@ for file in os.listdir("./cogs"):
 	if file.endswith(".py"):
 		bot.load_extension(f'cogs.{file[:-3]}') #[:-3] removes the last 3 chars
 
-
 @bot.event
 async def on_ready():
 	print (ascii_banner)
@@ -52,6 +80,16 @@ async def on_ready():
 	print ("Ver " + botver + " - check the updates with ?changelog")
 	print ("?setup to get started!")
 	print ("--------------------------------------------")
+
+	# Auto-update the server count to TOP.GG
+
+	dbltoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU5MTQ2NjkyMTgxMjE2NDYwOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTY1MjIwMDI1fQ.mlcCMiZ_0kPvxvLNiUbP8WLVpxsZ4Ll_zITVKkDjIMU' # set this to your DBL token
+	dblpy = dbl.DBLClient(client, dbltoken, autopost=True)
+
+	async def on_guild_post():
+		print("Server count posted successfully")
+
+
 	if botver != "":
 		game = discord.Game(botactivity[random.randrange(len(botactivity))] + " | v" + botver)
 	else:
@@ -61,16 +99,31 @@ async def on_ready():
 
 @bot.event
 async def on_guild_join(guild):
-	#NICE
 	srv.insert({'serverid': guild.id, 'heart': 'plus', 'crush': 'minus', 'star': '10'})
+	for channel in guild.text_channels:
+		if channel.permissions_for(guild.me).send_messages:
+			embed=discord.Embed(title="Thank you for inviting Reto!", description="Try using the ?setup command to get started!\nIf any problems arise, [join our Discord server](https://google.com) so we can give you a hand.")
+			embed.set_thumbnail(url="https://i.ibb.co/ySfQhDG/reto.png")
+			await channel.send(embed=embed)
+		break
 
 async def statusupdates():
 	while True:
 		await asyncio.sleep(30)
+
+		# update the activity list
+		activity.clear_cache()
+		getactivities = activity.all()
+		i = 0
+		botactivity = []
+		for value in getactivities:
+			botactivity.append(getactivities[i]["activity"])
+			i += 1
+
 		if botver != "":
 			game = discord.Game(botactivity[random.randrange(len(botactivity))] + " | v" + botver)
 		else:
 			game = discord.Game(botactivity)
-		await bot.change_presence(activity=game)
+		await bot.change_presence(activity=game)	
 
 bot.run(bottoken)
