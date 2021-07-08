cac9070063c08c70bd91a23803626515072b1fc1
Erik <hi@erik.games>
2021-07-08 11:57:56 -0300

    1.6b - Detailed comments for every database type

--- definitions.py
+++ definitions.py
@@ -64,10 +64,10 @@ def createConfigFile():
 	# Bot version
 	print("\nEnter BOT VERSION (6/9):")
 	print("The bot's current version. Appears on its Activities and other places.")
-	print("(default: 1.6a)")
+	print("(default: 1.6b)")
 	verInput = input("")
 	if not verInput:
-		verInput = "1.6a"
+		verInput = "1.6b"
 
 	# Bot owner
 	print("\nEnter BOT OWNER (7/9):")
@@ -121,12 +121,21 @@ for c in cfg:
 
 
 
-db           = TinyDB(encryption_key=key, path=join_path("db/","profile.reto"), storage=tae.EncryptedJSONStorage) #Database file: stores points of every user.
-srv          = TinyDB(encryption_key=key, path=join_path("db/","srv.reto"), storage=tae.EncryptedJSONStorage) #Server-specific configuration - allows you to modify stuff like the name of the reactions, for example.
-activity     = TinyDB(encryption_key=key, path=join_path("db/","activity.reto"), storage=tae.EncryptedJSONStorage) #Activity file: the "Playing" commands the bot has.
-post         = TinyDB(encryption_key=key, path=join_path("db/","comments.reto"), storage=tae.EncryptedJSONStorage) #Comment leaderboard.
-priv         = TinyDB(encryption_key=key, path=join_path("db/","blacklist.reto"), storage=tae.EncryptedJSONStorage) #Privacy Mode blacklist. Users with PM on will not have their messages logged in the comment leaderboard.
-best         = TinyDB(encryption_key=key, path=join_path("db/","best.reto"), storage=tae.EncryptedJSONStorage) #Best Of name: Used to look up the Best-Of name of the channel.
-dm           = TinyDB(encryption_key=key, path=join_path("db/","deletables.reto"), storage=tae.EncryptedJSONStorage) #Message deletion for Leaderboards.
-customprefix = TinyDB(encryption_key=key, path=join_path("db/","customprefix.reto"), storage=tae.EncryptedJSONStorage) #Prefix file: custom prefixes per server.
-chan         = TinyDB(encryption_key=key, path=join_path("db/","channels.reto"), storage=tae.EncryptedJSONStorage) #Channel file: Used for Autovotes and other channel specific features.
+db           = TinyDB(encryption_key=key, path=join_path("db/","profile.reto"), storage=tae.EncryptedJSONStorage)
+#Database file: stores points of every user.
+srv          = TinyDB(encryption_key=key, path=join_path("db/","srv.reto"), storage=tae.EncryptedJSONStorage)
+#Server-specific configuration - allows you to modify stuff like the name of the reactions, for example.
+activity     = TinyDB(encryption_key=key, path=join_path("db/","activity.reto"), storage=tae.EncryptedJSONStorage)
+#Activity file: the "Playing" commands the bot has.
+post         = TinyDB(encryption_key=key, path=join_path("db/","comments.reto"), storage=tae.EncryptedJSONStorage)
+#Comment file: every comment that's been reacted to is saved here.
+priv         = TinyDB(encryption_key=key, path=join_path("db/","blacklist.reto"), storage=tae.EncryptedJSONStorage)
+#Privacy Mode: Users with PM on will not have their messages logged in the comment leaderboard.
+best         = TinyDB(encryption_key=key, path=join_path("db/","best.reto"), storage=tae.EncryptedJSONStorage)
+#Best Of name: Used to look up the Best-Of name of the channel.
+dm           = TinyDB(encryption_key=key, path=join_path("db/","deletables.reto"), storage=tae.EncryptedJSONStorage)
+#Leaderboard logs: Database used to track the messages sent on scrolling leaderboards.
+customprefix = TinyDB(encryption_key=key, path=join_path("db/","customprefix.reto"), storage=tae.EncryptedJSONStorage)
+#Prefix file: Stores a server's custom prefix.
+chan         = TinyDB(encryption_key=key, path=join_path("db/","channels.reto"), storage=tae.EncryptedJSONStorage)
+#Channel file: Used for Autovote specific channels and other channel features.
